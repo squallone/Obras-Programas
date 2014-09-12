4cc93bc6cb006224cfe189079cb638831119f564
Pedro Contreras Nava <Pedro@MacBook-Pro-De-Pedro.local>
2014-09-12 16:30:06 -0500

    Cambio de fecha de modificación.

--- Obras-Programas/Classes/AppDelegate.m
+++ Obras-Programas/Classes/AppDelegate.m
@@ -2,7 +2,7 @@
 // Programador: Abdiel Soto, Pedro Contreras
 // Origen: Edicomex
 // Fecha inicio: Septiemnre 2014
-// Fecha ultima modificación: 23/08/2014
+// Fecha ultima modificación: 12/09/2014
 // Descripción:
 // Dependencias:
 
