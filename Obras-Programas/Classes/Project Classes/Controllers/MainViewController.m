7f59b8d1bba0b0fbe07a7d8a96becb614d27e2e4
abdielsba <squall25.10@hotmail.com>
2014-09-12 16:57:22 -0500

    Comentario en ViewDidLoad

--- Obras-Programas/Classes/Project Classes/Controllers/MainViewController.m
+++ Obras-Programas/Classes/Project Classes/Controllers/MainViewController.m
@@ -19,7 +19,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSLog(@"Welcome to Main ViewController");
+
 }
 
 - (void)didReceiveMemoryWarning {
